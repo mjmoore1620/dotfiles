4cd9c29b6c4ed86c7c9000883001d9946218fee8
matt moore <zmjm1320@gmail.com>
2026-05-10 19:55:17 -0400

    add lazyvim via lazy.nvim start

---
+++ nvim-lazyvim/lua/plugins/example.lua
@@ -0,0 +1,4 @@
+return {
+  -- This is a simple plugin to test if things are working
+  { "folke/which-key.nvim", opts = {} },
+}
--- shell/.bash_aliases
+++ shell/.bash_aliases
@@ -1,1 +1,2 @@
 alias nvk='NVIM_APPNAME="nvim-kickstart" nvim'
+alias nvl='NVIM_APPNAME="nvim-lazyvim" nvim'
